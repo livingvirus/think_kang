5ccdca88e11c04e683267533d505aabec5d7f243
livingvirus <livingviruskang@gmail.com>
2016-03-05 01:07:06 +0800

    初始化

---
+++ app/index/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+class Index
+{
+    public function index()
+    {
+    	return 'index_index';
+    }
+    public function test()
+    {
+    	return 'index_test';
+    }
+}
