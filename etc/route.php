c3027715814cd29894e60e4a5e932bb18572916f
livingkang <livingkang>
2016-04-13 17:38:22 +0800

    改用路由规则

--- etc/route.php
+++ etc/route.php
@@ -11,13 +11,5 @@
 // $Id$
 
 return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
-    '',
-
+    ''=>['','\\app\']
 ];
