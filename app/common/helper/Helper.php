78b1e101d73ea56b2a0ebfc547fd4a37e9a3ae76
livingviurs <livingviurs>
2016-05-31 00:08:29 +0800

    帮助函数写法

--- app/common/helper/Helper.php
+++ app/common/helper/Helper.php
@@ -3,12 +3,13 @@
 // ThinkPHP 助手函数
 //-------------------------
 // 获取配置参数
-function C($name = '', $value = null, $range = '') {
-	if (is_null($value) && is_string($name)) {
-		return \think\Config::get($name, $range);
-	} else {
-		return \think\Config::set($name, $value, $range);
-	}
+function Config($name = '', $value = null, $range = '')
+{
+    if (is_null($value) && is_string($name)) {
+        return \think\Config::get($name, $range);
+    } else {
+        return \think\Config::set($name, $value, $range);
+    }
 }
 
 /**
@@ -18,31 +19,33 @@ function C($name = '', $value = null, $range = '') {
  * @param integer $dec 小数位
  * @return mixed
  */
-function G($start, $end = '', $dec = 6) {
-	if ('' == $end) {
-		\think\Debug::remark($start);
-	} else {
-		return 'm' == $dec ? \think\Debug::getRangeMem($start, $end) : \think\Debug::getRangeTime($start, $end, $dec);
-	}
+function Debug($start, $end = '', $dec = 6)
+{
+    if ('' == $end) {
+        \think\Debug::remark($start);
+    } else {
+        return 'm' == $dec ? \think\Debug::getRangeMem($start, $end) : \think\Debug::getRangeTime($start, $end, $dec);
+    }
 }
 
-function session($name, $value = '') {
-	if (is_array($name)) {
-		// 初始化
-		\think\Session::init($name);
-	} elseif (is_null($name)) {
-		// 清除
-		\think\Session::clear($value);
-	} elseif ('' === $value) {
-		// 获取
-		return \think\Session::get($name);
-	} elseif (is_null($value)) {
-		// 删除session
-		return \think\Session::delete($name);
-	} else {
-		// 设置session
-		return \think\Session::set($name, $value);
-	}
+function Session($name, $value = '')
+{
+    if (is_array($name)) {
+        // 初始化
+        \think\Session::init($name);
+    } elseif (is_null($name)) {
+        // 清除
+        \think\Session::clear($value);
+    } elseif ('' === $value) {
+        // 获取
+        return \think\Session::get($name);
+    } elseif (is_null($value)) {
+        // 删除session
+        return \think\Session::delete($name);
+    } else {
+        // 设置session
+        return \think\Session::set($name, $value);
+    }
 }
 
 /**
@@ -52,29 +55,30 @@ function session($name, $value = '') {
  * @param mixed $options 缓存参数
  * @return mixed
  */
-function S($name, $value = '', $options = null) {
-	if (is_array($options)) {
-		// 缓存操作的同时初始化
-		\think\Cache::connect($options);
-	} elseif (is_array($name)) {
-		// 缓存初始化
-		return \think\Cache::connect($name);
-	}
-	if ('' === $value) {
-		// 获取缓存
-		return \think\Cache::get($name);
-	} elseif (is_null($value)) {
-		// 删除缓存
-		return \think\Cache::rm($name);
-	} else {
-		// 缓存数据
-		if (is_array($options)) {
-			$expire = isset($options['expire']) ? $options['expire'] : null; //修复查询缓存无法设置过期时间
-		} else {
-			$expire = is_numeric($options) ? $options : null; //默认快捷缓存设置过期时间
-		}
-		return \think\Cache::set($name, $value, $expire);
-	}
+function Cache($name, $value = '', $options = null)
+{
+    if (is_array($options)) {
+        // 缓存操作的同时初始化
+        \think\Cache::connect($options);
+    } elseif (is_array($name)) {
+        // 缓存初始化
+        return \think\Cache::connect($name);
+    }
+    if ('' === $value) {
+        // 获取缓存
+        return \think\Cache::get($name);
+    } elseif (is_null($value)) {
+        // 删除缓存
+        return \think\Cache::rm($name);
+    } else {
+        // 缓存数据
+        if (is_array($options)) {
+            $expire = isset($options['expire']) ? $options['expire'] : null; //修复查询缓存无法设置过期时间
+        } else {
+            $expire = is_numeric($options) ? $options : null; //默认快捷缓存设置过期时间
+        }
+        return \think\Cache::set($name, $value, $expire);
+    }
 }
 
 /**
@@ -83,10 +87,11 @@ function S($name, $value = '', $options = null) {
  * @param string $level 日志级别
  * @return void|array
  */
-function trace($log = '[think]', $level = 'log') {
-	if ('[think]' === $log) {
-		return \think\Log::getLog();
-	} else {
-		\think\Log::record($log, $level);
-	}
+function Log($log = '[think]', $level = 'log')
+{
+    if ('[think]' === $log) {
+        return \think\Log::getLog();
+    } else {
+        \think\Log::record($log, $level);
+    }
 }
